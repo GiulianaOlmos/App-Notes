ce373cfe36584bad9884ba2248d550f74a422492
Giuliana Evangelina Olmos <giulianaolmos@gmail.com>
2020-06-30 20:37:58 -0300

    Conexion con database

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+require('dotenv').config(); //Si existe un archivo llamdo .env va leer lo que tiene dentro y lo asigna en las variables de entorno. 
+
 const app = require('./server');
+require('./database');
+
 
 app.listen(app.get('port'), () => {
     console.log('Server on port:', app.get('port'))
