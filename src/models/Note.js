141e0dc66d873d77fc984819647ffcff6b935a65
Giuliana Evangelina Olmos <giulianaolmos@gmail.com>
2020-08-19 15:57:20 -0300

    Notas publicas a privadas

--- src/models/Note.js
+++ src/models/Note.js
@@ -8,7 +8,11 @@ const NoteSchema = new Schema({
     description: {
         type: String,
         require: true,
-    }
+    },
+    user: {
+        type: String,
+        required: true,
+    },
 },{
     timestamps:true,
 })
